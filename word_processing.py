f7ce19b67d757982a71d21ecfdefd802c9cc8241
Trevor Brown <browntj1@uci.edu>
2023-02-01 00:08:57 -0800

    Implemented create_data_folder, save_word_count, and tally_top_50_words

--- word_processing.py
+++ word_processing.py
@@ -1,22 +1,24 @@
 import re
-import sys
-from bs4 import BeautifulSoup
 import json
+import pathlib
+
+stop_list = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an',
+            'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before',
+            'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could',
+            "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down',
+            'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't",
+            'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's",
+            'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've",
+            'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't",
+            'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours']
 
 def tokenize(link_text):
     token_list = []
     ok_chars = r"[^A-Za-z0-9]" # Regular expression to only accept alphanumeric characters
     expression = re.compile(ok_chars)
-    try:
-        for line in file:
-            fixed_line = expression.sub(' ', line) # Remove non alphanumeric characters from the line
-            for token in fixed_line.split():
-                token_list.append(token.lower())
-    except:
-        print(f"File Failed to Read {text_file_path}: File not text file")
-        file.close()
-        return []
-    file.close() # Close file
+    fixed_text = expression.sub(' ', link_text) # Remove non alphanumeric characters from the text
+    for token in fixed_text.split():
+        token_list.append(token.lower())
     return token_list
 
 def compute_word_frequencies(token_list):
@@ -28,8 +30,44 @@ def compute_word_frequencies(token_list):
             frequencies[token] = 1
     return frequencies
 
-def print_frequencies(frequencies):
-    pair_list = [ (-frequencies[key], key) for key in frequencies] # list comprehension to create list of reversed tuples for each token, value is negative for proper sorting
-    pair_list.sort() # Sorts the tuples in reverse order for their values and forward order for their keys
+
+def create_data_folder():
+    # Creates the data folder or overwrites it if it already exists
+    pathlib.Path("data").mkdir(exist_ok = True)
+    pathlib.Path("data/word_counts.txt").touch(exist_ok=True)
+    pathlib.Path("data/top_50_words.txt").touch(exist_ok=True)
+
+def save_word_count(frequencies):
+    file = open("data/word_counts.txt", "a")
+    json.dump(frequencies, file)
+    file.write("\n")
+    file.close()
+
+def tally_top_50_words():
+    word_counts_file = open("data/word_counts.txt", "r")
+    output_file = open("data/top_50_words.txt", "a")
+    freq_totals = {}
+    for line in word_counts_file:
+        freq = json.loads(line)
+        for key in freq:
+            if key in freq_totals:
+                freq_totals[key] += freq[key]
+            else:
+                freq_totals[key] = freq[key]
+    # list comprehension to create list of reversed tuples for each token, value is negative for proper sorting
+    pair_list = [ (-freq_totals[key], key) for key in freq_totals] 
+    pair_list.sort()
+    count = 1
     for value, key in pair_list:
-        print(f"<{key}> = <{-value}>")
+        if count > 50:
+            break
+        if key in stop_list:
+            pass
+        else:
+            output_file.write(f"{count}: Word: <{key}> Total: <{-value}>\n")
+            count += 1
+
+def count_unique_links():
+    pass
+    
+    
