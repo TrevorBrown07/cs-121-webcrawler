a2fdc70876a806df854ea98126bf95e4e84e5097
Trevor Brown <browntj1@uci.edu>
2023-02-01 00:58:23 -0800

    Implemented compute_page_length, save_page_length, and find_longest_page

--- word_processing.py
+++ word_processing.py
@@ -14,7 +14,7 @@ stop_list = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'a
 
 def tokenize(link_text):
     token_list = []
-    ok_chars = r"[^A-Za-z0-9]" # Regular expression to only accept alphanumeric characters
+    ok_chars = r"[^A-Za-z0-9']" # Regular expression to only accept alphanumeric characters
     expression = re.compile(ok_chars)
     fixed_text = expression.sub(' ', link_text) # Remove non alphanumeric characters from the text
     for token in fixed_text.split():
@@ -30,13 +30,37 @@ def compute_word_frequencies(token_list):
             frequencies[token] = 1
     return frequencies
 
-
 def create_data_folder():
     # Creates the data folder or overwrites it if it already exists
     pathlib.Path("data").mkdir(exist_ok = True)
     pathlib.Path("data/word_counts.txt").touch(exist_ok=True)
+    pathlib.Path("data/page_lengths.txt").touch(exist_ok=True)
     pathlib.Path("data/report.txt").touch(exist_ok=True)
     pathlib.Path("data/valid.txt").touch(exist_ok=True)
+
+def compute_page_length(token_list):
+    count = 0
+    for token in token_list:
+        count += 1
+    return count
+
+def save_page_length(page_lengths):
+    file = open("data/page_lengths.txt", "a")
+    json.dump(page_lengths)
+    file.close()
+
+def find_longest_page():
+    page_lengths_file = open("data/page_lengths.txt", "r")
+    report = open("data/report.txt", "a")
+    current_longest = ("placeholder", 0)
+    for line in page_lengths_file:
+        pl_pair = json.loads(line)
+        for key in pl_pair:
+            if pl_pair[key] > current_longest[1]:
+                current_longest = (key, pl_pair[key])
+    report.write(f"Longest Page by number of words: {current_longest} \n\n")
+    page_lengths_file.close()
+    report.close()
 
 def save_word_count(frequencies):
     file = open("data/word_counts.txt", "a")
@@ -46,8 +70,8 @@ def save_word_count(frequencies):
 
 def tally_top_50_words():
     word_counts_file = open("data/word_counts.txt", "r")
-    output_file = open("data/report.txt", "a")
-    output_file.write("Top 50 Words in the domains: \n")
+    report = open("data/report.txt", "a")
+    report.write("Top 50 Words in the domains: \n")
     freq_totals = {}
     for line in word_counts_file:
         freq = json.loads(line)
@@ -66,21 +90,21 @@ def tally_top_50_words():
         if key in stop_list:
             pass
         else:
-            output_file.write(f"{count}: Word: <{key}> Total: <{-value}> \n")
+            report.write(f"{count}: Word: <{key}> Total: <{-value}> \n")
             count += 1
-    output_file.write("\n")
+    report.write("\n")
     word_counts_file.close()
-    output_file.close()
+    report.close()
 
 def count_unique_links():
     url_file = open("data/valid.txt", 'r')
-    url_count_file = open("data/report.txt", "a")
+    report = open("data/report.txt", "a")
     url_count = 0
     for line in url_file:
         url_count += 1
-    url_count_file.write(f"Total number of unique URLs: {url_count} \n\n")
+    report.write(f"Total number of unique URLs: {url_count} \n\n")
     url_file.close()
-    url_count_file.close()
+    report.close()
     
 
 def count_subdomains():
