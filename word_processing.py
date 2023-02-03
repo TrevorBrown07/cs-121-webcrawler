10431e18a11445d93474b7bbd362fcc8a16c40a9
Trevor Brown <browntj1@uci.edu>
2023-02-03 02:20:16 -0800

    Reworked multiple functions in word_processing

--- word_processing.py
+++ word_processing.py
@@ -2,8 +2,11 @@ import re
 import json
 import pathlib
 from urllib.parse import urlparse
-
-stop_list = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an',
+import hashlib
+import csv
+from collections import Counter
+from collections import defaultdict
+stop_list = set(['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an',
             'and', 'any', 'are', "aren't", 'as', 'at', 'be', 'because', 'been', 'before',
             'being', 'below', 'between', 'both', 'but', 'by', "can't", 'cannot', 'could',
             "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't", 'down',
@@ -11,16 +14,17 @@ stop_list = ['a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'a
             'have', "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's",
             'hers', 'herself', 'him', 'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've",
             'if', 'in', 'into', 'is', "isn't", 'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't",
-            'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours']
+            'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours',
+            'ourselves','out','over','own','same',"shan't",'she',"she'd","she'll","she's","should","shouldn't",'so','some',
+            'such','than','that',"that's","the","their","theirs","them","themselves","then","there","there's",'these',
+            'they',"they'd","they'll","they're","they've",'this','those','through','to','too','under','until','up','very',
+            'was',"wasn't",'we',"we'd","we'll","we're","we've",'were',"weren't",'what',"what's",'when',"where's",'which',
+            'while','who',"who's",'whom','why',"why's",'with',"won't",'would',"wouldn't",'you',"you'd","you'll","you're","you've",
+            "your","yours",'yourself','yourselves'])
 
-def tokenize(link_text):
-    token_list = []
-    ok_chars = r"[^A-Za-z0-9']" # Regular expression to only accept alphanumeric characters
-    expression = re.compile(ok_chars)
-    fixed_text = expression.sub(' ', link_text) # Remove non alphanumeric characters from the text
-    for token in fixed_text.split():
-        token_list.append(token.lower())
-    return token_list
+def tokenize(text):
+    ALPHANUMERIC = r'\b([0-9a-zA-Z]+)\b'
+    return [word.lower() for word in re.findall(ALPHANUMERIC,text)]
 
 def compute_word_frequencies(token_list):
     frequencies = {}
@@ -31,100 +35,121 @@ def compute_word_frequencies(token_list):
             frequencies[token] = 1
     return frequencies
 
+def compute_hash(token_list):
+    string = ""
+    for i in range(min(10,len(token_list))):
+        string+= token_list[i]
+    string+=str(len(token_list))
+    return hashlib.md5(string.encode('utf-8')).hexdigest()
+
 def create_data_folder():
     # Creates the data folder or overwrites it if it already exists
     pathlib.Path("data").mkdir(exist_ok = True)
-    pathlib.Path("data/word_counts.txt").touch(exist_ok=True)
-    pathlib.Path("data/page_lengths.txt").touch(exist_ok=True)
-    pathlib.Path("data/report.txt").touch(exist_ok=True)
     pathlib.Path("data/valid.txt").touch(exist_ok=True)
+    with open('data/report.txt', 'w', newline='') as f:
+        f.write("")
+    pathlib.Path("data/testing.csv").touch(exist_ok=True)
+    with open('data/testing.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Link", "Length", "Hash","Frequencies"])
+    pathlib.Path("data/valid.txt").touch(exist_ok=True)
+    with open('data/valid.txt', 'w', newline='') as f:
+        f.write("")
     pathlib.Path("data/invalid.txt").touch(exist_ok=True)
+    with open('data/invalid.txt', 'w', newline='') as f:
+        f.write("")
+
+def write_csv(first,second,third,fourth):
+    with open('data/testing.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([first,second,third,fourth])
+
 
 def compute_page_length(token_list):
     return len(token_list)
 
-def save_page_length(page_lengths):
-    file = open("data/page_lengths.txt", "a")
-    json.dump(page_lengths, file)
-    file.write("\n")
-    file.close()
-
 def find_longest_page():
-    page_lengths_file = open("data/page_lengths.txt", "r")
-    report = open("data/report.txt", "a")
-    current_longest = ("placeholder", 0)
-    for line in page_lengths_file:
-        pl_pair = json.loads(line)
-        for key in pl_pair:
-            if pl_pair[key] > current_longest[1]:
-                current_longest = (key, pl_pair[key])
-    report.write(f"Longest Page by number of words: {current_longest} \n\n")
-    page_lengths_file.close()
-    report.close()
+    columns = defaultdict(list)
+    with open("data/testing.csv","r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for (k,v) in row.items():
+                if k == "Length":
+                    columns[k].append(int(v))
+                else:
+                    columns[k].append(v)
+                    
+    maxIndex = columns["Length"].index(max(columns["Length"]))
 
-def save_word_count(frequencies):
-    file = open("data/word_counts.txt", "a")
-    json.dump(frequencies, file)
-    file.write("\n")
-    file.close()
+    with open("data/report.txt", "a") as f:
+        f.write(f"\nLongest page found: {columns['Link'][maxIndex]} {columns['Length'][maxIndex]} \n\n")
+
 
 def tally_top_50_words():
-    word_counts_file = open("data/word_counts.txt", "r")
-    report = open("data/report.txt", "a")
-    report.write("Top 50 Words in the domains: \n")
-    freq_totals = {}
-    for line in word_counts_file:
-        freq = json.loads(line)
-        for key in freq:
-            if key in freq_totals:
-                freq_totals[key] += freq[key]
-            else:
-                freq_totals[key] = freq[key]
-    # list comprehension to create list of reversed tuples for each token, value is negative for proper sorting
-    pair_list = [ (-freq_totals[key], key) for key in freq_totals] 
-    pair_list.sort()
-    count = 1
-    for value, key in pair_list:
-        if count > 50:
-            break
-        if key in stop_list:
-            pass
-        else:
-            report.write(f"{count}: Word: <{key}> Total: <{-value}> \n")
+    top50 = Counter()
+
+    columns = defaultdict(list)
+    with open("data/testing.csv","r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for (k,v) in row.items():
+                if k == "Frequencies":
+                    top50.update(Counter(json.loads(v)))
+
+    answer = ""
+    count = 0
+    for i in top50.most_common():
+        if i[0] not in stop_list and len(i[0]) != 1:
+            answer += (str(i) + "\n")
             count += 1
-    report.write("\n")
-    word_counts_file.close()
-    report.close()
+        if count >= 50:
+            break
+    
+    with open("data/report.txt", "a") as f:
+        f.write(f"Top 50 words:\n{answer}\n")
+    
 
 def count_unique_links():
-    url_file = open("data/valid.txt", 'r')
-    report = open("data/report.txt", "a")
-    url_count = 0
-    for line in url_file:
-        url_count += 1
-    report.write(f"Total number of unique URLs: {url_count} \n\n")
-    url_file.close()
-    report.close()
+    totalLinks = set()
+    columns = defaultdict(list)
+    with open("data/testing.csv","r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for (k,v) in row.items():
+                if k == "Link":
+                    totalLinks.add(v.replace(urlparse(v).fragment,""))
+    with open("data/report.txt", "a") as f:
+        f.write(f"Total number of unique URLs: {len(totalLinks)}\n")
+        for i,v in enumerate(totalLinks,1):
+            f.write("["+str(i)+"] "+v)
+            f.write("\n")
+
     
 def count_subdomains():
-    url_file = open("data/valid.txt", 'r')
-    report = open("data/report.txt", "a")
-    subdomains = {"ics.uci.edu": 0}
-    for line in url_file:
-        url = urlparse(line)
-        domain = re.sub("^www.", "", url.netloc)
-        if domain in subdomains:
-            subdomains[domain] += 1
-        elif(re.match(".+\.ics.uci.edu$", domain)):
-            subdomains[domain] = 1
-    report.write("ics.uci.edu subdomains: \n")
-    for domain in subdomains:
-        report.write(f"{domain}, {subdomains[domain]}\n")
-    report.write("\n")
-    url_file.close()
-    report.close()
+    totalLinks = defaultdict(set)
+    columns = defaultdict(list)
+    with open("data/testing.csv","r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            for (k,v) in row.items():
+                parsed = urlparse(v)
+                if k == "Link" and re.search(r"ics.uci.edu",parsed.netloc):
+                    totalLinks[parsed.scheme+"://"+parsed.netloc].add(v.replace(parsed.fragment,""))
+    answer = ""
+    for i,v in totalLinks.items():
+        answer += i + " "+str(len(v))+"\n"
+        count = 1
+        for j in v:
+            answer += " ["+str(count)+"] "+j
+            count += 1
+        answer += "\n\n"
+        
+    with open("data/report.txt", "a") as f:
+        f.write(f"Subdomains:{len(totalLinks)}\n{answer}\n\n")
 
 def create_report():
+    with open("data/report.txt", "w") as f:
+        f.write("")
     count_unique_links()
     find_longest_page()
     tally_top_50_words()
